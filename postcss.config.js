c11181e9de0d99a9a3d83a7cd5d4d5b11ad37352
SonOfMagic <qq1324318532@gmail.com>
2023-02-27 16:26:46 +0800

    feat: commit deviceRatio

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,13 @@ module.exports = {
     autoprefixer: {},
     'postcss-pxtransform':{
       platform: 'weapp',
-      designWidth: 750,
+      designWidth: 375,
+      deviceRatio :{
+        '640': 2.34 / 2,
+        '750': 1,
+        '828': 1.81 / 2,
+        '375': 2 / 1
+      }
     },
     'postcss-rem-to-responsive-pixel': {
       rootValue: 32,
