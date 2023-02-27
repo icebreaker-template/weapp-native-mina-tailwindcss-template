a6b2b9212d6a32384bd7c554f5cdfc715ddb31b7
SonOfMagic <qq1324318532@gmail.com>
2023-02-27 13:15:25 +0800

    feat: 提交px->rpx 1:1转化

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-pxtransform':{
+      platform: 'weapp',
+      designWidth: 750,
+    },
     'postcss-rem-to-responsive-pixel': {
       rootValue: 32,
       propList: ['*'],
