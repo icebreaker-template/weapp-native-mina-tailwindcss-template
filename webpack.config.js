1679b1620bdcb5c855fc121702262d2c1ab52f1f
SonOfMagic <qq1324318532@gmail.com>
2023-05-19 13:39:23 +0800

    chore: use weapp-tailwindcss/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MinaWebpackPlugin = require('./plugin/MinaWebpackPlugin')
 const MinaRuntimePlugin = require('./plugin/MinaRuntimePlugin')
 const LodashWebpackPlugin = require('lodash-webpack-plugin')
-const { UnifiedWebpackPluginV5 } = require('weapp-tailwindcss-webpack-plugin')
+const { UnifiedWebpackPluginV5 } = require('weapp-tailwindcss/webpack')
 const debuggable = process.env.BUILD_TYPE !== 'release'
 
 module.exports = {
