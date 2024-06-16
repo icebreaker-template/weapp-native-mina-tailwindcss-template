18fc41ee8c737337e935903a830ad22d159f349b
ice breaker <1324318532@qq.com>
2024-06-16 21:41:00 +0800

    chore(deps): upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{wxml,js,ts}'],
   theme: {
-    extend: {}
+    extend: {},
   },
   plugins: [],
   corePlugins: {
-    preflight: false
-  }
+    preflight: false,
+  },
 }
